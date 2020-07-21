7e3001bd636491ed6ab0743a08250a64c19c9611
SagittariusA <nilsole2009@live.no>
2020-07-21 15:43:51 +0200

    Now subtract is running, and runs through all of the file by default

--- mapeditor.py
+++ mapeditor.py
@@ -12,7 +12,7 @@ class Atlas:
         self.jk_choices   = ["odde", "dayn", "half", "sdlb"]
         self.jk           = ["odde"]
         self.jack         = False
-        self.tool_choices   = ["coadd", "subtract", "divide", "coaddfeed, scale"]
+        self.tool_choices   = ["coadd", "subtract"]
         self.tool           = "coadd"
         self.freq         = "all"
         self.det_list     = np.arange(1,20)
@@ -36,8 +36,14 @@ class Atlas:
         if self.outfile == None:
             print("To save result to outfile, please provide a valid outfile name!")
             sys.exit() 
+        if self.jack and ("jackknives" not in self.dfile1 or "jackknives" not in self.dfile2):
+            print("One or both of the input files does not contain any jackknife information!")
+            sys.exit()
         if not self.full and not self.beam and not self.jack:
             self.everything = True
+            if "jackknives" in self.dfile1 and "jackknives" in self.dfile2:
+                nhit_lst = [i for i in self.dfile1["jackknives"].keys() if "nhit_" in i]
+                self.jk =  [i.split("_")[1] for i in nhit_lst]
 
         self.operation()
         self.dfile1.close()
@@ -180,7 +186,7 @@ class Atlas:
         print("-l ----> optional --deepy"     + wrapper.fill(m16))
         sys.exit()
 
-    def readMap(self, first_file = True, jackmode = "odde"):
+    def readMap(self, first_file = True, jackmode = None):
         if first_file:
             dfile = self.dfile1
         else:
@@ -191,7 +197,7 @@ class Atlas:
         sb_start = self.sb_list[0] - 1
         sb_end   = self.sb_list[-1] - 1
 
-        if self.jack:
+        if jackmode != None:
             if jackmode == "dayn" or jackmode == "half":
                 map  =  dfile["jackknives/map_" + jackmode][:, self.det_list - 1, sb_start:sb_end + 1, freq_start:freq_end + 1, ...]
                 nhit =  dfile["jackknives/nhit_" + jackmode][:, self.det_list - 1, sb_start:sb_end + 1, freq_start:freq_end + 1, ...]
@@ -200,19 +206,18 @@ class Atlas:
                 map  =  dfile["jackknives/map_" + jackmode][:, sb_start:sb_end + 1, freq_start:freq_end + 1, ...]
                 nhit =  dfile["jackknives/nhit_" + jackmode][:, sb_start:sb_end + 1, freq_start:freq_end + 1, ...]
                 rms  =  dfile["jackknives/rms_" + jackmode][:, sb_start:sb_end + 1, freq_start:freq_end + 1, ...]
-            
         else:
             if self.beam:
                 map =  dfile["map_beam"][sb_start:sb_end + 1, freq_start:freq_end + 1, ...]
                 nhit =  dfile["nhit_beam"][sb_start:sb_end + 1, freq_start:freq_end + 1, ...]
                 rms =  dfile["rms_beam"][sb_start:sb_end + 1, freq_start:freq_end + 1, ...]
-            else:
+            elif self.full:
                 map =  dfile["map"][self.det_list - 1, sb_start:sb_end + 1, freq_start:freq_end + 1, ...]
                 nhit =  dfile["nhit"][self.det_list - 1, sb_start:sb_end + 1, freq_start:freq_end + 1, ...]
                 rms =  dfile["rms"][self.det_list - 1, sb_start:sb_end + 1, freq_start:freq_end + 1, ...]
         return map, nhit, rms
         
-    def writeMap(self, jackmode = None):
+    def writeMap(self, jackmode = None, write_the_rest = False):
         if jackmode != None:
             map_name    = "jackknives/map_" + jackmode
             nhit_name   = "jackknives/nhit_" + jackmode
@@ -228,12 +233,13 @@ class Atlas:
                 self.ofile.create_dataset(map_name, data = self.map)
                 self.ofile.create_dataset(nhit_name, data = self.nhit)
                 self.ofile.create_dataset(rms_name, data = self.rms)
-        else:
+
+        elif self.beam or self.full:
             if self.beam:
                 map_name    = "map_beam"
                 nhit_name   = "nhit_beam"
                 rms_name    = "rms_beam"
-            else: 
+            elif self.full: 
                 map_name    = "map"
                 nhit_name   = "nhit"
                 rms_name    = "rms"
@@ -249,10 +255,85 @@ class Atlas:
                 self.ofile.create_dataset(map_name, data = self.map)
                 self.ofile.create_dataset(nhit_name, data = self.nhit)
                 self.ofile.create_dataset(rms_name, data = self.rms)
-        
+        if write_the_rest:
+            data_not_to_copy = ["jackknives", "map", "map_beam", "nhit", 
+                                "nhit_beam", "rms", "rms_beam"]
+            jk_data_not_to_copy = ["map_dayn",  "map_half",  "map_odde",  "map_sdlb",
+                                   "nhit_dayn", "nhit_half", "nhit_odde", "nhit_sdlb",
+                                   "rms_dayn",  "rms_half",  "rms_odde",  "rms_sdlb"]
+            for name in self.dfile1.keys():
+                if name not in self.ofile.keys() and name not in data_not_to_copy:
+                    self.ofile.create_dataset(name, data = self.dfile1[name])    
+            
+            if "jackknives" in self.dfile1 and "jackknives" in self.dfile2 and "jackknives" in self.ofile:
+                for name in self.dfile1["jackknives"].keys():
+                    if name not in self.ofile["jackknives"].keys() and name not in jk_data_not_to_copy:
+                        self.ofile.create_dataset("jackknives/" + name, 
+                                                    data = self.dfile1["jackknives/" + name])   
+
     def operation(self):                
         if self.infile1 != None and self.infile2 != None:
-            if self.jack:
+            if self.everything:
+                if "jackknives" in self.dfile1 and "jackknives" in self.dfile2:
+                    for jack in self.jk:
+                        self.map1, self.nhit1, self.rms1 = self.readMap(True, jack)
+                        self.map2, self.nhit2, self.rms2 = self.readMap(False, jack)
+                        
+                        if self.tool == "coadd":
+                            if len(self.map1.shape) == 6:
+                                self.C_coadd6D(self.map1, self.nhit1, self.rms1,
+                                              self.map2, self.nhit2, self.rms2)  
+                    
+                            elif len(self.map1.shape) == 5: 
+                                self.C_coadd5D(self.map1, self.nhit1, self.rms1,
+                                              self.map2, self.nhit2, self.rms2)  
+
+                        elif self.tool == "subtract": 
+                            self._map   = self.subtract(self.map1, self.map2)
+                            self._nhit   = self.subtract(self.nhit1, self.nhit2)
+                            self._rms   = self.add_rms(self.rms1, self.rms2)
+                            if len(self.map1.shape) == 6:
+                                self.C_subtract6D(self.map1, self.nhit1, self.rms1,
+                                              self.map2, self.nhit2, self.rms2)  
+                            
+                            if len(self.map1.shape) == 5:
+                                self.C_subtract5D(self.map1, self.nhit1, self.rms1,
+                                              self.map2, self.nhit2, self.rms2) 
+                            print(np.allclose(self._map, self.map))
+                            print(np.allclose(self._nhit, self.nhit))
+                            print(np.allclose(self._rms, self.rms))
+                        self.writeMap(jack)
+                
+                self.full = True
+                self.map1, self.nhit1, self.rms1 = self.readMap(True)
+                self.map2, self.nhit2, self.rms2 = self.readMap(False)
+                
+                if self.tool == "coadd":
+                    self.C_coadd5D(self.map1, self.nhit1, self.rms1,
+                                   self.map2, self.nhit2, self.rms2)
+
+                elif self.tool == "subtract": 
+                    self.map   = self.subtract(self.map1, self.map2)
+                    self.nhit   = self.subtract(self.nhit1, self.nhit2)
+                    self.rms   = self.add_rms(self.rms1, self.rms2)
+                self.writeMap()
+                
+                self.full = False
+                self.beam = True
+                self.map1, self.nhit1, self.rms1 = self.readMap(True)
+                self.map2, self.nhit2, self.rms2 = self.readMap(False)
+                
+                if self.tool == "coadd":
+                    self.C_coadd4D(self.map1, self.nhit1, self.rms1,
+                                   self.map2, self.nhit2, self.rms2)
+
+                elif self.tool == "subtract": 
+                    self.map   = self.subtract(self.map1, self.map2)
+                    self.nhit   = self.subtract(self.nhit1, self.nhit2)
+                    self.rms   = self.add_rms(self.rms1, self.rms2)
+                self.writeMap()
+            
+            elif self.jack:
                 for jack in self.jk:
                     self.map1, self.nhit1, self.rms1 = self.readMap(True, jack)
                     self.map2, self.nhit2, self.rms2 = self.readMap(False, jack)
@@ -264,14 +345,19 @@ class Atlas:
                 
                         elif len(self.map1.shape) == 5: 
                             self.C_coadd5D(self.map1, self.nhit1, self.rms1,
-                                           self.map2, self.nhit2, self.rms2)  
+                                           self.map2, self.nhit2, self.rms2)
 
                     elif self.tool == "subtract": 
-                        self.map   = self.subtract(self.map1, self.map2)
-                        self.nhit   = self.subtract(self.nhit1, self.nhit2)
-                        self.rms   = self.subtract_rms(self.rms1, self.rms2)
+                        if len(self.map1.shape) == 6:
+                            self.C_subtract6D(self.map1, self.nhit1, self.rms1,
+                                        self.map2, self.nhit2, self.rms2)  
+                        
+                        if len(self.map1.shape) == 5:
+                            self.C_subtract5D(self.map1, self.nhit1, self.rms1,
+                                        self.map2, self.nhit2, self.rms2) 
                     self.writeMap(jack)
-            else:
+
+            elif self.full or self.beam:
                 self.map1, self.nhit1, self.rms1 = self.readMap(True)
                 self.map2, self.nhit2, self.rms2 = self.readMap(False)
                 
@@ -288,47 +374,7 @@ class Atlas:
                     self.nhit   = self.subtract(self.nhit1, self.nhit2)
                     self.rms   = self.subtract_rms(self.rms1, self.rms2)
                 self.writeMap()
-                
-
-        """
-            self.data1 = self.readMap(self.infile1, mode = self.map_mode)
-            self.data2 = self.readMap(self.infile2, mode = self.map_mode)
-            self.hits1 = self.readMap(self.infile1, mode = "nhit")
-            self.hits2 = self.readMap(self.infile2, mode = "nhit")
-            self.mask = self.hits1 * self.hits2 > 0
-            self.data1 = np.where(self.mask, self.data1, 0)
-            self.data2 = np.where(self.mask, self.data2, 0)
-
-            if self.map_mode == "map":
-                if self.tool == "add":
-                    self.data = self.add(self.data1, self.data2)
-
-                elif self.tool == "coadd":
-                    rms1 = self.readMap(self.infile1, mode = "rms")
-                    rms2 = self.readMap(self.infile2, mode = "rms")
-                    self.data = np.zeros_like(rms1)
-                    self.data[self.mask] = self.coadd(self.data1[self.mask], 1 / rms1[self.mask],
-                                                      self.data2[self.mask], 1 / rms2[self.mask])
-
-                elif self.tool == "subtract": 
-                    self.data   = self.subtract(self.data1, self.data2)
-
-            elif self.map_mode == "nhit":
-                if self.tool == "add":
-                    self.data = self.add(self.hits1, self.hits2)
-
-                elif self.tool == "subtract": 
-                    self.data = self.subtract(self.hits1, self.hits2)
-
-            elif self.map_mode == "rms":
-                self.data = np.zeros_like(self.data1)
-                if self.tool == "coadd":
-                    self.data[self.mask] = self.add_rms(1 / self.data1[self.mask],
-                                                        1 / self.data2[self.mask])
-                elif self.tool == "subtract": 
-                    self.data[self.mask] = self.subtract_rms(1 / self.data1[self.mask], 
-                                                             1 / self.data2[self.mask])
-        """
+        self.writeMap(write_the_rest = True)
 
     def add(self, data1, data2):
         return data1 + data2
@@ -402,32 +448,69 @@ class Atlas:
                                  self.map, self.nhit, self.rms,
                                  n0,       n1,        n2, 
                                  n3,       n4,        n5)
-                            
 
-    def add_rms(self, inv_rms1, inv_rms2):
-        inv_var1 = np.square(inv_rms1)
-        inv_var2 = np.square(inv_rms2)
-        sum = inv_var1 + inv_var2 
-        sum = 1 / np.sqrt(sum)
-        return sum
-
-    def subtract_rms(self, inv_rms1, inv_rms2):
-        var_inv1 = np.square(inv_rms1)
-        var_inv2 = np.square(inv_rms2)
-        sum = var_inv1 - var_inv2 
-        sum = 1 / np.sqrt(sum)
-        return sum
+    def C_subtract4D(self, map1, nhit1, rms1,
+                           map2, nhit2, rms2):
+        float32_array4 = np.ctypeslib.ndpointer(dtype=ctypes.c_float, ndim=4, flags="contiguous")
+        int32_array4 = np.ctypeslib.ndpointer(dtype=ctypes.c_int, ndim=4, flags="contiguous")
+        self.maputilslib.subtract4D.argtypes = [float32_array4, int32_array4, float32_array4,
+                                                float32_array4, int32_array4, float32_array4,
+                                                float32_array4, int32_array4, float32_array4,
+                                                ctypes.c_int,   ctypes.c_int, ctypes.c_int,
+                                                ctypes.c_int]
+        n0, n1, n2, n3  = self.map1.shape
+        self.map        = np.zeros_like(map1,   dtype = ctypes.c_float)
+        self.nhit       = np.zeros_like(nhit1,  dtype = ctypes.c_int)
+        self.rms        = np.zeros_like(rms1,   dtype = ctypes.c_float)
+        self.maputilslib.subtract4D(map1, nhit1, rms1,
+                                 map2, nhit2, rms2, 
+                                 self.map, self.nhit, self.rms,
+                                 n0, n1, n2, n3)
     
-    def coaddfeed(self, data, inv_rms):
-        inv_var = np.square(inv_rms)
-        coadded = np.sum(map * inv_var, axis = 0)
-        coadded /= np.sum(inv_var, axis = 0)
-        return coadded
 
-    def coaddfeed_rms(self, inv_rms):
-        inv_var = np.square(inv_rms)
-        coadded = np.sum(inv_var, axis = 0)
-        return coadded
+    def C_subtract5D(self, map1, nhit1, rms1,
+                           map2, nhit2, rms2):
+        float32_array5 = np.ctypeslib.ndpointer(dtype=ctypes.c_float, ndim=5, flags="contiguous")
+        int32_array5 = np.ctypeslib.ndpointer(dtype=ctypes.c_int, ndim=5, flags="contiguous")
+        self.maputilslib.subtract5D.argtypes = [float32_array5, int32_array5, float32_array5,
+                                                float32_array5, int32_array5, float32_array5,
+                                                float32_array5, int32_array5, float32_array5,
+                                                ctypes.c_int,   ctypes.c_int, ctypes.c_int,
+                                                ctypes.c_int,   ctypes.c_int]
+        n0, n1, n2, n3, n4 = self.map1.shape
+        self.map = np.zeros_like(map1, dtype = ctypes.c_float)
+        self.nhit = np.zeros_like(nhit1, dtype = ctypes.c_int)
+        self.rms = np.zeros_like(rms1, dtype = ctypes.c_float)
+        self.maputilslib.subtract5D(map1, nhit1, rms1,
+                                 map2, nhit2, rms2, 
+                                 self.map, self.nhit, self.rms,
+                                 n0, n1, n2, n3, n4)
+                            
+    def C_subtract6D(self, map1, nhit1, rms1,
+                           map2, nhit2, rms2):
+        float32_array6 = np.ctypeslib.ndpointer(dtype=ctypes.c_float, ndim=6, flags="contiguous")
+        int32_array6 = np.ctypeslib.ndpointer(dtype=ctypes.c_int, ndim=6, flags="contiguous")
+        self.maputilslib.subtract6D.argtypes = [float32_array6, int32_array6, float32_array6,
+                                                float32_array6, int32_array6, float32_array6,
+                                                float32_array6, int32_array6, float32_array6,
+                                                ctypes.c_int,   ctypes.c_int, ctypes.c_int,
+                                                ctypes.c_int,   ctypes.c_int, ctypes.c_int]
+        n0, n1, n2, n3, n4, n5 = self.map1.shape
+        self.map = np.zeros_like(map1, dtype = ctypes.c_float)
+        self.nhit = np.zeros_like(nhit1, dtype = ctypes.c_int)
+        self.rms = np.zeros_like(rms1, dtype = ctypes.c_float)
+        self.maputilslib.subtract6D(map1,     nhit1,     rms1,
+                                 map2,     nhit2,     rms2, 
+                                 self.map, self.nhit, self.rms,
+                                 n0,       n1,        n2, 
+                                 n3,       n4,        n5)
+                            
+    def add_rms(self, rms1, rms2):
+        var1 = np.square(rms1)
+        var2 = np.square(rms2)
+        sum = var1 + var2 
+        sum = np.sqrt(sum)
+        return sum
 
 
 if __name__ == "__main__":
--- maputilslib.c
+++ maputilslib.c
@@ -40,7 +40,6 @@ void coadd4D(float* map1, int* nhit1, float* rms1,
     }
 }
 
-
 void coadd5D(float* map1, int* nhit1, float* rms1, 
              float* map2, int* nhit2, float* rms2,
              float* map,  int* nhit,  float* rms,
@@ -119,47 +118,117 @@ void coadd6D(float* map1, int* nhit1, float* rms1,
             }
         }
     }
- }
+}
 
+void subtract4D(float* map1, int* nhit1, float* rms1, 
+                float* map2, int* nhit2, float* rms2,
+                float* map,  int* nhit,  float* rms,
+                int n0,      int n1,     int n2, 
+                int n3                
+                ){
+    int prod = n2 * n3;
+    int prod1 = prod * n1;
+    int idx;
+    float var1, var2;
 
-/*
-void add5D(float* mapout, float* mapin1, float* mapin2, int n0, int n1,
-              int n2, int n3, int n4){
+    for(int i=0; i < n0; i++){
+        for(int j=0; j < n1; j++){
+            for(int k=0; k < n2; k++){
+                for(int l=0; l < n3; l++){
+                    idx = prod1 * i + prod * j + n3 * k + l;
+                    if (nhit1[idx] * nhit2[idx] > 0){
+                        var1  = rms1[idx] * rms1[idx];
+                        var2  = rms2[idx] * rms2[idx];
+                        map[idx]  = map1[idx] - map2[idx];
+
+                        nhit[idx] = nhit1[idx] + nhit2[idx];
+                        rms[idx]  =  sqrt(var1 + var2);
+                    }
+                    else {
+                        map[idx] = 0;
+                        nhit[idx] = 0;
+                        rms[idx] = 0;
+                    }       
+                }
+            }
+        }
+    }
+}
+
+void subtract5D(float* map1, int* nhit1, float* rms1, 
+             float* map2, int* nhit2, float* rms2,
+             float* map,  int* nhit,  float* rms,
+             int n0,      int n1,     int n2, 
+             int n3,      int n4                
+             ){
+    int prod = n3 * n4;
+    int prod1 = prod * n2;
+    int prod2 =  prod1 * n1;
+    int idx;
+    float var1, var2;
+
     for(int i=0; i < n0; i++){
         for(int j=0; j < n1; j++){
             for(int k=0; k < n2; k++){
                 for(int l=0; l < n3; l++){
                     for(int m=0; m < n4; m++){
-                        int idx = (n4 * (n3 * (n2 * (i*n1 + j) + k) + l) + m);
-                        mapout[idx] = mapin1[idx] + mapin2[idx];
+                        idx = prod2 * i + prod1 * j + prod * k + n4 * l + m;
+                        if (nhit1[idx] * nhit2[idx] > 0){
+                            var1        = rms1[idx] * rms1[idx];
+                            var2        = rms2[idx] * rms2[idx];
+                            map[idx]    = map1[idx] - map2[idx];
+
+                            nhit[idx]   = nhit1[idx] + nhit2[idx];
+                            rms[idx]    =  sqrt(var1 + var2);
+                        }
+                        else {
+                            map[idx]    = 0;
+                            nhit[idx]   = 0;
+                            rms[idx]    = 0;
+                        }       
                     }
                 }
             }
         }
     }
-    
-    clock_t t = clock();
-    t = clock() - t;
-    
-    double time_taken = ((double) t)/ CLOCKS_PER_SEC;
-    printf("Run time in C: %g sec\n", time_taken);
 }
-*/
-    /*
-    Python:
-        float32_array5 = np.ctypeslib.ndpointer(dtype=ctypes.c_float, ndim=5, flags="contiguous")
-        self.maputilslib.add5D.argtypes = [float32_array5, float32_array5, float32_array5,
-                                    ctypes.c_int, ctypes.c_int, ctypes.c_int,
-                                    ctypes.c_int, ctypes.c_int]
-        n0, n1, n2, n3, n4 = map1.shape
-        map = np.zeros((n0, n1, n2, n3, n4), dtype = ctypes.c_float)
-        start = time.time()
-        self.maputilslib.add5D(map, map1, map2, n0, n1, n2, n3, n4)
-        print("Run time outside C: ", time.time() - start, " sec")
 
-        start = time.time()
-        sum = map1 + map2
-        print("Run time python/numpy: ", time.time() - start, " sec")
-        print(np.allclose(map, sum))
-        
-    */
+void subtract6D(float* map1, int* nhit1, float* rms1, 
+             float* map2, int* nhit2, float* rms2,
+             float* map,  int* nhit,  float* rms,
+             int n0,      int n1,     int n2, 
+             int n3,      int n4,     int n5){
+    int prod = n4 * n5;
+    int prod1 = prod * n3;
+    int prod2 =  prod1 * n2;
+    int prod3 =  prod2 * n1;
+    int idx;
+    float var1, var2;
+
+    for(int i=0; i < n0; i++){
+        for(int j=0; j < n1; j++){
+            for(int k=0; k < n2; k++){
+                for(int l=0; l < n3; l++){
+                    for(int m=0; m < n4; m++){
+                        for(int n=0; n < n5; n++){
+                            idx = prod3 * i + prod2 * j + prod1 * k + prod * l + n5 * m + n;
+                            if (nhit1[idx] * nhit2[idx] > 0){
+                                var1  = rms1[idx] * rms1[idx];
+                                var2  = rms2[idx] * rms2[idx];
+                                map[idx]  = map1[idx] - map2[idx];
+
+                                nhit[idx] = nhit1[idx] + nhit2[idx];
+                                rms[idx]  =  sqrt(var1 + var2);
+                            }
+                            else {
+                                map[idx] = 0;
+                                nhit[idx] = 0;
+                                rms[idx] = 0;
+                            }
+                        }      
+                    }
+                }
+            }
+        }
+    }
+ }
