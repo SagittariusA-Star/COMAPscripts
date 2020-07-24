fd460571ea8c787f7302e31a8bf941197d21d354
SagittariusA <nilsole2009@live.no>
2020-07-24 09:25:36 +0200

    developed ugrade algos and implemented them in C

--- udgrade_test.py
+++ udgrade_test.py
@@ -254,19 +254,18 @@ A = A.reshape(3, 3, 3, 3, 3, 3)
 numXY = 2
 numZ = 2
 n0, n1, n2, n3, n4, n5 = A.shape 
-N3, N4, N5 = n3 * numXY, n4 * numXY, n5 * numXY
-B = np.zeros((n0, n1, n2, N3, N4, N5))
+N3 = n3 * numZ
+B = np.zeros((n0, n1, n2, N3, n4, n5))
 
 for q in range(n0):
-    for l in range(n1):
-        for k in range(n2):
-            for p in range(n3):
-                for a in range(n4):
-                    for b in range(n5):
-                        for z in range(p * numXY, (p + 1) * numXY):
-                            for i in range(a * numXY, (a + 1) * numXY):
-                                for j in range(b * numXY, (b + 1) * numXY):
-                                    B[q, l, k, z, i, j] = A[q, l, k, p, a, b]
+    for h in range(n1):
+        for p in range(n2):
+            for k in range(n3):
+                for i in range(n4):
+                    for j in range(n5):
+                        for z in range(k * numZ, (k + 1) * numZ):
+                            B[q, h, p, z, i, j] = A[q, h, p, k, i, j]
+
 print(A[0, 0, 0, 0, :, :], "\n")
 print(A[0, 0, 0, 1, :, :], "\n")
 print(A[0, 0, 0, 2, :, :], "\n")
@@ -277,3 +276,52 @@ print(B[0, 0, 0, 3, :, :], "\n")
 print(B[0, 0, 0, 4, :, :], "\n")
 print(B[0, 0, 0, 5, :, :])
 
+
+m_l = np.arange(3 * 3 * 3 * 3 * 3 * 3)
+m_l = m_l.reshape(3, 3, 3, 3, 3, 3).astype(ctypes.c_float)
+
+n_l = np.arange(3 * 3 * 3 * 3 * 3 * 3)
+n_l = m_l.reshape(3, 3, 3, 3, 3, 3).astype(ctypes.c_int)
+
+r_l = np.arange(3 * 3 * 3 * 3 * 3 * 3)
+r_l = m_l.reshape(3, 3, 3, 3, 3, 3).astype(ctypes.c_float)
+
+numZ = int(2)
+numXY = int(2)
+n0, n1, n2, n3, n4, n5 = m_l.shape
+N3 = n3 * numZ
+
+m_h = np.zeros((n0, n1, n2, N3, n4, n5), dtype = ctypes.c_float)
+n_h = np.zeros((n0, n1, n2, N3, n4, n5), dtype = ctypes.c_int)
+r_h = np.zeros((n0, n1, n2, N3, n4, n5), dtype = ctypes.c_float)
+
+
+maputilslib = ctypes.cdll.LoadLibrary("maputilslib.so.1")  # Load shared library
+
+float32_array6 = np.ctypeslib.ndpointer(dtype=ctypes.c_float, ndim=6, flags="contiguous")
+int32_array6 = np.ctypeslib.ndpointer(dtype=ctypes.c_int, ndim=6, flags="contiguous")
+maputilslib.ugradeZ6D.argtypes = [float32_array6, int32_array6, float32_array6,
+                                   float32_array6, int32_array6, float32_array6,
+                                   ctypes.c_int,   ctypes.c_int, ctypes.c_int,
+                                   ctypes.c_int,   ctypes.c_int, ctypes.c_int,
+                                   ctypes.c_int,   ctypes.c_int]
+print(m_l.shape, m_h.shape)
+
+maputilslib.ugradeZ6D(m_h, n_h,  r_h,
+                        m_l, n_l,  r_l,
+                        n0,  n1,   n2,
+                        n3,  n4,   n4,
+                        N3,  numZ)
+
+print(np.allclose(A, n_l))
+print(np.allclose(B, n_h))
+
+print(m_l[0, 0, 0, 0, :, :], "\n")
+print(m_l[0, 0, 0, 1, :, :], "\n")
+print(m_l[0, 0, 0, 2, :, :], "\n")
+print(m_h[0, 0, 0, 0, :, :], "\n")
+print(m_h[0, 0, 0, 1, :, :], "\n")
+print(m_h[0, 0, 0, 2, :, :], "\n")
+print(m_h[0, 0, 0, 3, :, :], "\n")
+print(m_h[0, 0, 0, 4, :, :], "\n")
+print(m_h[0, 0, 0, 5, :, :])
