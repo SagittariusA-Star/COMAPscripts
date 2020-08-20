29bb7bf332cd97c8276982162e78e8036eac9e60
SagittariusA <nilsole2009@live.no>
2020-08-20 19:18:15 +0200

    Testing different convolutions

--- test_convolution.py
+++ test_convolution.py
@@ -8,12 +8,12 @@ import matplotlib.pyplot as plt
 
 np.random.seed(123345)
 
-a = np.random.randint(0, 9, (10, 10, 10, 201, 201))
-a[:, :, :, 50, :] = np.linspace(0, 50, 201)
-a[:, :, :, :, 50] = np.linspace(0, -50, 201)
-a[:, :, :, :, 75] = 50 * np.sin(np.linspace(0, -50, 201))
+a = np.random.randint(0, 9, (80, 10, 10, 101, 101))
+a[:, :, :, 50, :] = np.linspace(0, 50, 101)
+a[:, :, :, :, 50] = np.linspace(0, -50, 101)
+a[:, :, :, :, 75] = 50 * np.sin(np.linspace(0, -50, 101))
 
-a = a.reshape(10, 10 * 10, 201, 201)
+a = a.reshape(80, 10 * 10, 101, 101)
 
 def gaussian_kernel(sigma_x, sigma_y, n_sigma=5.0):
     size_y = int(n_sigma * sigma_y)
@@ -61,14 +61,30 @@ def gaussian_smoothXYZ(mymap, sigma_x, sigma_y, sigma_z, n_sigma = 5.0):
     #[len(mymap.shape)-2, len(mymap.shape) - 1])
     return smoothed_map
 
+def gaussian_smoothXYZ_2(mymap, sigma_x, sigma_y, sigma_z, n_sigma = 5.0):
+    kernel = gaussian_kernelXYZ(sigma_x, sigma_y, sigma_z, n_sigma = n_sigma)
+    smoothed_map = signal.convolve(mymap, kernel[np.newaxis, :, :, :], mode='same', method = "auto")
+    #[len(mymap.shape)-2, len(mymap.shape) - 1])
+    return smoothed_map
 
+def gaussian_smoothZ_2(mymap, sigma_z, n_sigma=5.0):
+    kernel = gaussian_kernelZ(sigma_z, n_sigma=n_sigma)
+    smoothed_map = signal.convolve(mymap, kernel[np.newaxis, :, np.newaxis, np.newaxis], mode='same', method = "auto")
+    #[len(mymap.shape)-2, len(mymap.shape) - 1])
+    return smoothed_map
 
 b = gaussian_smoothXYZ(a, 5, 5, 1)
 
+bXY = gaussian_smooth(a, 5, 5)
+print(bXY.shape, a.shape)
+#bXYZ = gaussian_smoothZ(bXY, 1)
+
+c = gaussian_smoothXYZ_2(a, 5, 5, 1)
+print(np.allclose(b, bXY))
 fig, ax = plt.subplots(1, 2)
-ax[0].imshow(a[5, :, :, 0].T)
+ax[0].imshow(a[0, 0, :, :].T)
 ax[0].set_title("Test map")
-ax[1].imshow(b[5, :, :, 0].T)
+ax[1].imshow(b[0, 0, :, :].T)
 ax[1].set_title("Smoothed")
 plt.savefig("test_smoothed_map.png")
 plt.show()
